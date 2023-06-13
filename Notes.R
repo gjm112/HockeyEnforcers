0e038c0cfeb30511e52713ef33e073999483c30a
Gregoery J. Matthews <gregorymatthews@gregorys-mbp.lan>
2023-06-12 21:21:54 -0500

    Report update.

---
+++ Notes.R
@@ -0,0 +1,9 @@
+#Notes from meeting. 
+What is it that we want to estimate?  
+Is it one more fight? 
+
+Plots of Age at deaths vs fights.  (Zach thinks logarithmic?)
+
+#CoxPH models 
+1. Keep number of fights continuous (probably non-linear). 
+2. Choose their cut point.  
